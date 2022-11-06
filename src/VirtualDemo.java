c34ff29f02d6769968f3025ca2eacc49e6ed0578
nurharzeki <90377761+nurharzeki@users.noreply.github.com>
2022-11-06 21:18:18 +0700

    Update VirtualDemo.java

--- src/VirtualDemo.java
+++ src/VirtualDemo.java
@@ -21,3 +21,30 @@ public class VirtualDemo {
         e.mailcheck();
     }
 }
+
+
+//========== HASIL RUN PROGRAM ============
+/*
+Menyusun Pegawai
+Menyusun Pegawai
+
+Memanggil mailcheck berdasarkan referensi gaji--
+Memeriksa kelas gaji dalam surat
+Surat tertuju untuk Wahyu dengan gaji 5000.0
+
+Memanggil mailcheck berdasarkan referensi pegawai--
+Memeriksa kelas gaji dalam surat
+Surat tertuju untuk Ini Nama dengan gaji 2500.0
+
+*/
+
+
+//========= ANALISIS HASIL RUN ==========
+/*
+Menyusun Pegawai muncul 2 kali dikarenakan perintah untuk mencetak kalimat tersebut akan dijalankan setiap kali membuat object kelas gaji atau pegawai
+bagian pertama bernama wahyu dengan gaji 5000 karena di ambil dari referensi kelas gaji (DOWNCASTING)
+bagian kedua bernama ini nama dengan gaji 2500 karena di ambil dari kelas pegawai (UPCASTING)
+
+Polimorphisme terdapat pada object e (pembuatan object kelas pegawai dengan bentuk kelas gaji
+
+*/
